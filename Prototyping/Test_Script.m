7f123b1fd58c8b90197622544c41702137161fe7
wawjordan <wajordan@vt.edu>
2021-04-22 18:33:34 -0400

    Updated format to test new classes

--- Prototyping/Test_Script.m
+++ Prototyping/Test_Script.m
@@ -1,22 +1,41 @@
 %% Test Script
 clc; clear; close all;
-% fprintf('Blah blah blah\n')
-Ncells = 128;
+
+Ncells = 200;
 xi = linspace(-1,2,Ncells+1);
 inputs = struct();
 inputs.n_ghost = 2;
-inputs.dt = 0.1;
-inputs.time_range = [0,1];
-%inputs.IC = @(x) sin(pi*x);
+inputs.dt = 0.01;
+inputs.time_range = [0,1.5];
 soln = inviscid_burgers1D(xi,inputs);
+flux = fluxes('scheme','eo');
+RK = RK_Explicit('Method','Euler');
+
 hold on;
-t = 0;
-for i =1:10
-plot(soln.grid.xc,soln.calc_exact(soln.grid.xc,t),'.')
-t = t + soln.dt;
+N = length(inputs.time_range(1):inputs.dt:inputs.time_range(2));
+for i =1:N
+    clf;
+    soln.t = soln.t + soln.dt;
+    left = soln.U(soln.i_low-1:soln.i_high);
+    right = soln.U(soln.i_low:soln.i_high+1);
+    soln.F = flux.calc_flux(left,right);
+    soln.R = soln.residual(soln.F);
+%     soln.U(soln.i) = soln.U(soln.i)-(1./soln.grid.dx(soln.i)).*soln.R*soln.dt;
+    soln.U = RK.eval(@(t,u)burgers(t,u,flux,soln),soln.U,soln.t,soln.dt);
+    hold on;
+    plot(soln.grid.xc(soln.i),soln.U(soln.i),'r')
+    plot(soln.grid.xc,soln.calc_exact(soln.grid.xc,soln.t),'k')
+    
+    hold off;
+    pause(0.01);
 end
-hold off;
-xlim([-1,2])
-ylim([-1,2])
 
 
+function dUdt = burgers(~,U,flux,soln)
+    dUdt = zeros(length(U),1);
+    left = U(soln.i_low-1:soln.i_high);
+    right = U(soln.i_low:soln.i_high+1);
+    F = flux.calc_flux(left,right);
+    R = soln.residual(F);
+    dUdt(soln.i) = -(1./soln.grid.dx(soln.i)).*R;
+end
