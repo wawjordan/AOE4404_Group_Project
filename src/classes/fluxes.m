fd1273017c7feb41d2988592ed6eef01db55132d
wawjordan <wajordan@vt.edu>
2021-04-23 18:27:07 -0400

    fixed expansion shock correction in Roe's flux (I think)

--- src/classes/fluxes.m
+++ src/classes/fluxes.m
@@ -46,9 +46,11 @@ classdef fluxes
             a_bar = 0.5*(right+left);
             eps = max(0,max((a_bar-left),(right-a_bar)));
             a_bar_mod1 = abs(a_bar);
-            a_bar_mod2 = 0.5*((a_bar.^2)./eps + eps);
-            a_bar_mod2(isnan(a_bar_mod2)|isinf(a_bar_mod2)) = 0;
-            a_bar_mod = a_bar_mod1.*(a_bar>=eps) + a_bar_mod2.*(a_bar<eps);
+            a_bar_mod2 = eps;
+%             a_bar_mod2 = 0.5*((a_bar.^2)./eps + eps);
+%             a_bar_mod2(isnan(a_bar_mod2)|isinf(a_bar_mod2)) = 0;
+            a_bar_mod = a_bar_mod1.*(a_bar_mod1>=eps) ...
+                + a_bar_mod2.*(a_bar_mod1<eps);
             FL = 0.5*left.^2;
             FR = 0.5*right.^2;
             F = 0.5*(FL + FR) - 0.5*a_bar_mod.*(right-left);
