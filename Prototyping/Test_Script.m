25116aa35628a80a69fded0980f623889766a11d
wawjordan <wajordan@vt.edu>
2021-04-27 17:56:11 -0400

    mucked about

--- Prototyping/Test_Script.m
+++ Prototyping/Test_Script.m
@@ -5,41 +5,38 @@ Ncells = 16;
 n_ghost = 2;
 xi = linspace(-1,1,Ncells+1);
 inputs = struct();
-inputs.dt = 0.001;
+inputs.dt = 0.01;
 inputs.time_range = [0,0.5];
 inputs.order = 2;
 inputs.kappa = 1;
-inputs.exact_solution_type = 'initial_linear';
+inputs.exact_solution_type = 'initial_disc';
 
-inputs.uLeft = 1;
-inputs.uRight = -1;
+inputs.uLeft = -1;
+inputs.uRight = 1;
 soln = inviscid_burgers1D(xi,n_ghost,inputs);
 flux = fluxes('scheme','godunov');
 limiter = limiters('scheme','van_leer');
 % limiter = limiters('scheme','beta_lim','beta',1);
 RK = RK_Explicit('Method','RK41');
 
-hold on;
 N = length(inputs.time_range(1):inputs.dt:inputs.time_range(2));
+
 for i =1:N
-    clf;
     soln.t = soln.t + soln.dt;
 %     left = soln.U(soln.i_low-1:soln.i_high);
 %     right = soln.U(soln.i_low:soln.i_high+1);
-    [left,right] = MUSCL_extrap(soln,limiter);
-    soln.F = flux.calc_flux(left,right);
-    soln.R = soln.residual(soln.F);
+%     [left,right] = MUSCL_extrap(soln,limiter);
+%     soln.F = flux.calc_flux(left,right);
+%     soln.R = soln.residual(soln.F);
 %     soln.U(soln.i) = soln.U(soln.i)-(1./soln.grid.dx(soln.i)).*soln.R*soln.dt;
     soln.U = RK.eval(@(t,u)burgers(t,u,flux,limiter,soln),soln.U,soln.t,soln.dt);
     soln.E = soln.U(soln.i)-soln.calc_exact(soln.grid.xc(soln.i),soln.t);
-    hold on;
-    plot(soln.grid.xc(soln.i),soln.E,'r')
-%     plot(soln.grid.xc(soln.i),soln.U(soln.i),'r')
-%     plot(soln.grid.xc,soln.calc_exact(soln.grid.xc,soln.t),'k')
-    xlim([soln.grid.xmin,soln.grid.xmax]);
-    hold off;
-    pause(0.01);
 end
+hold on;
+plot(soln.grid.xc(soln.i),soln.U(soln.i),'r')
+plot(soln.grid.xc,soln.calc_exact(soln.grid.xc,soln.t),'k')
+xlim([soln.grid.xmin,soln.grid.xmax]);
+hold off;
 
 
 function dUdt = burgers(~,U,flux,limiter,soln)
