05ce7e4d913db9e8dc506ca5de24b8f92c3eaeb7
wawjordan <wajordan@vt.edu>
2021-05-05 13:55:19 -0400

    Changed stopping criterion to be based on function value

--- src/functions/newton_safe.m
+++ src/functions/newton_safe.m
@@ -3,11 +3,13 @@ xk = nan(maxiter,1);
 ea = nan(maxiter,1);
 
 xk(1) = x0;
-xk(2) = xk(1) - f(xk(1))/df(xk(1));
+fk1 = f(xk(1));
+xk(2) = xk(1) - fk1/df(xk(1));
 k = 2;
-ea(k) = abs(xk(k)-xk(k-1))/abs(xk(k));
+ea(k) = 1;
 while (ea(k)>tol)&&(k <= maxiter)
-    xk(k+1) = xk(k) - f(xk(k))/df(xk(k));
+    fk = f(xk(k));
+    xk(k+1) = xk(k) - fk/df(xk(k));
     if ( xk(k+1) < a )||( xk(k+1) > b)
         xk(k+1) = a +(b-a)/2;
         if sign(f(a)) == sign(f(xk(k+1)))
@@ -16,7 +18,7 @@ while (ea(k)>tol)&&(k <= maxiter)
             b = xk(k+1);
         end
     end
-    ea(k+1) = abs(xk(k+1)-xk(k))/abs(xk(k+1));
+    ea(k+1) = abs(fk)/abs(fk1);
     k = k + 1;
 end
 x = xk(k);
