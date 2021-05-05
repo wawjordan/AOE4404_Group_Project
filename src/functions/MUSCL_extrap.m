8c8fafbf8215beae62b8808ac317391120bc524c
wawjordan <wajordan@vt.edu>
2021-05-05 13:54:43 -0400

    Added boundary condition enforcement

--- src/functions/MUSCL_extrap.m
+++ src/functions/MUSCL_extrap.m
@@ -1,4 +1,4 @@
-function [left,right] = MUSCL_extrap(soln,limiter)
+function [left,right] = MUSCL_extrap(soln,limiter,bc)
     k = soln.kappa_MUSCL;
     e = soln.epsilon_MUSCL;
     
@@ -7,11 +7,7 @@ function [left,right] = MUSCL_extrap(soln,limiter)
     r_minus = 0.0*soln.U;
     i = soln.i_low-1:soln.i_high;
     
-    i_low2 = soln.i_low:soln.i_low+soln.grid.n_ghost-1;
-    i_high2 = soln.i_high-soln.grid.n_ghost+1:soln.i_high;
-    
-    soln.U(soln.i_high+1:end) = soln.U(i_low2);
-    soln.U(1:soln.i_low-1) = soln.U(i_high2);
+    soln.U = bc(soln,soln.U);
     
     den = soln.U(i+1) - soln.U(i);
     den = sign(den).*max(abs(den),delta);
@@ -21,19 +17,15 @@ function [left,right] = MUSCL_extrap(soln,limiter)
     r_plus(i) = num./den;
     num = soln.U(i) - soln.U(i-1);
     r_minus(i) = num./den;
-    
-%     r_plus(soln.i_low-2) = r_plus(soln.i_low-1);
-%     r_minus(soln.i_low-2) = r_minus(soln.i_low-1);
-% 
-%     r_plus(soln.i_high+1) = r_plus(soln.i_high);
-%     r_minus(soln.i_high+1) = r_minus(soln.i_high);
-
-    r_plus(soln.i_low-2) = r_plus(soln.i_high);
-    r_minus(soln.i_low-2) = r_minus(soln.i_high);
-
-    r_plus(soln.i_high+1) = r_plus(soln.i_low);
-    r_minus(soln.i_high+1) = r_minus(soln.i_low);
-
+    if strcmp(func2str(bc),'periodic_bc')
+        r_plus = periodic_bc(soln,r_plus);
+        r_minus = periodic_bc(soln,r_minus);
+    else
+        r_plus(soln.i_low-2) = r_plus(soln.i_low-1);
+        r_minus(soln.i_low-2) = r_minus(soln.i_low-1);
+        r_plus(soln.i_high+1) = r_plus(soln.i_high);
+        r_minus(soln.i_high+1) = r_minus(soln.i_high);
+    end
     
     psi_plus = limiter.limit(r_plus);
     psi_minus = limiter.limit(r_minus);
