7e9c12ef56eac58f212be3d04e6d55233ec2cd56
wawjordan <wajordan@vt.edu>
2021-05-09 15:27:20 -0400

    Various changes

--- Prototyping/Batch_test_script.m
+++ Prototyping/Batch_test_script.m
@@ -4,12 +4,12 @@ clc; clear; close all;
 % fluxess = {'godunov','eo','roe'};
 fluxess = {'roe'};
 limiterss = {'van_leer','van_albada','minmod','beta_lim'};
-RK = RK_Explicit('Method','RK41');
+RK = RK_Explicit('Method','Euler');
 BC = @periodic_bc;
 
 Ncells = 2.^(4:10);
 M = length(Ncells);
-dt = 0.001*2.^(0:-1:-(M-1));
+dt = 0.01*2.^(0:-1:-(M-1));
 
 
 K = length(limiterss);
@@ -23,9 +23,9 @@ for ii = 1:K
 end
 
 inputs = struct();
-inputs.time_range = [0,0.1];
+inputs.time_range = [0,0.5];
 inputs.order = 2;
-inputs.kappa = -1.0;
+inputs.kappa = 1;
 input.beta = 1.5;
 inputs.exact_solution_type = 'initial_sine';
 % inputs.uLeft = -1;
@@ -38,6 +38,12 @@ labels = {'van Leer limiter','van Albada limiter','minmod limiter',...
 
 
 E = struct();
+intervals = (inputs.time_range(2)-inputs.time_range(1))./dt;
+out_interval = 1;
+for i = 1:10
+    out_interval = max(out_interval,gcd(i,intervals(1)));
+end
+intervals = intervals/out_interval;
 for j = 1:K
 for i = 1:M
 fprintf('Limiter=%s    N = %d     (%d/%d)\n',labels{j},Ncells(i),(j-1)*M+i,M*K);
@@ -51,15 +57,19 @@ end
 inputs.dt = dt(i);
 xi = linspace(0,1,Ncells(i)+1);
 % xi = linspace(-1,2,Ncells(i)+1);
-soln = solve_burgers(xi,inputs,flux,limiter,RK,BC);
-E(i,j).x = soln.grid.xc(soln.i);
+[soln,out] = solve_burgers(xi,inputs,flux,limiter,RK,BC,intervals(i));
+E(i,j).t = out.t;
+E(i,j).x = out.x;
 E(i,j).dx = min(soln.grid.dx);
-E(i,j).error = soln.E;
-E(i,j).norm1 = sum(abs(soln.E))/Ncells(i);
-E(i,j).norm2 = sqrt(sum((soln.E).^2)/Ncells(i));
-E(i,j).norminf = max(abs(soln.E));
-E(i,j).soln = soln.U(soln.i);
-E(i,j).exsoln = soln.Uex;
+E(i,j).dt = dt(i);
+E(i,j).flux = flux.scheme;
+E(i,j).limiter = limiter.scheme;
+E(i,j).error = out.E;
+E(i,j).norm1 = out.norm1;
+E(i,j).norm2 = out.norm2;
+E(i,j).norminf = out.norminf;
+E(i,j).soln = out.U;
+E(i,j).exsoln = out.Uex;
 end
 end
 %% Plotting
@@ -85,14 +95,15 @@ set(ax1,'DefaultTextFontSize',8);
 
 hold on;
 for j=1:K
-plot([E(:,j).dx],[E(:,j).norm1],mark{j});
+    e1 = [E(:,j).norm1];
+plot([E(:,j).dx],e1(end,:),mark{j});
 end
 hold off;
 set(gca,'xscale','log')
 set(gca,'yscale','log')
 xlabel('$\Delta{x}$','interpreter','latex')
 ylabel('$\Vert{\epsilon}\Vert_1$','interpreter','latex','rotation',0)
-ylim([1e-4,1e-1])
+ylim([1e-6,1e-1])
 pbaspect([1.2 1 1])
 
 
@@ -107,13 +118,16 @@ set(ax2,'DefaultTextFontSize',8);
 r = Ncells(2:end)./Ncells(1:end-1);
 hold on;
 for j=1:K
-eex = [E(1:M-1,j).norm1]./[E(2:M,j).norm1];
+    e1 = [E(1:M-1,j).norm1];
+    e2 = [E(2:M,j).norm1];
+    eex = e1(end,:)./e2(end,:);
+% eex = [E(1:M-1,j).norm1(end)]./[E(2:M,j).norm1(end)];
 px = log(eex)./log(r);
 plot([E(2:end,j).dx],px,mark{j})
 end
 hold off;
 set(gca,'xscale','log')
-ylim([0,3])
+ylim([0,4])
 xlim(get(ax1,'xlim'));
 xlabel('$\Delta{x}$','interpreter','latex')
 ylabel('$\hat{p}\quad\quad$','interpreter','latex','rotation',0)
@@ -122,11 +136,6 @@ pbaspect([1.2 1 1])
 
 dirname1 = 'C:\Users\Will Jordan\Documents\MATLAB\Grad_School\AOE4404\Project\Results\Data\';
 dirname2 = 'C:\Users\Will Jordan\Documents\MATLAB\Grad_School\AOE4404\Project\Results\Figures\';
-% filename = sprintf('sine_test_int=%s',RK.Method);
-filename = sprintf('sine_test_int=%s',RK.Method);
-% filename = 'flux=EO_limiter=vary_int=Heun';
-filename = [dirname1,filename];
-save([dirname1,filename],E);
+filename = 'test';%sprintf('test=%s_flux=%s_kap%0.1f_int=%s_BC=%s_dt0=%0.5f',inputs.exact_solution_type,fluxess{1},inputs.kappa,RK.Method,func2str(BC),dt(1));
+save([dirname1,filename],'E');
 exportgraphics(gcf,[dirname2,filename,'.png'],'Resolution',600)
-% print(filename,'-dpng','-r600');
-% crop(strcat(filename,'.png'))
